cc4cfca88c3cd8d47ab877c1698d17e6598fb27d
shane newsom <shaned@pm.me>
2025-08-08 14:02:27 -0500

    Spelling fix and right fix.  50/50.

--- VibeHome.Domain/Entities/User.cs
+++ VibeHome.Domain/Entities/User.cs
@@ -5,9 +5,9 @@ namespace VibeHome.Domain.Entities
         public int UserId { get; set; }
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty; // 'Parent', 'Partner', or 'Child'
+        public string Role { get; set; } = string.Empty; // 'parent', or 'Child'
         public bool IsDeleted { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
     }
-} 
+}
