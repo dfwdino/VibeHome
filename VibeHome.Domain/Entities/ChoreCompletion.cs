3cfe68ce66c67225f5effc69815368cc95f40d18
shane newsom <shaned@pm.me>
2025-11-24 16:59:41 -0600

    Fixxed some bugs with the list of chorse. I sitll need to fix the name b/c the update is not correct. :-)

--- VibeHome.Domain/Entities/ChoreCompletion.cs
+++ VibeHome.Domain/Entities/ChoreCompletion.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VibeHome.Domain.Entities
 {
     public class ChoreCompletion
     {
         public int ChoreCompletionId { get; set; }
+        [Required(ErrorMessage = "Please select a kid")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a kid")]
         public int KidId { get; set; }
         public int ChoreItemId { get; set; }
+
+        [Required(ErrorMessage = "Please select a Location")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a Location")]
         public int LocationId { get; set; }
         public DateTime CompletionDateTime { get; set; }
         public string? Notes { get; set; }
@@ -18,25 +25,5 @@ namespace VibeHome.Domain.Entities
         public KidsChoreLocation? Location { get; set; }
     }
 
-    /// <summary>
-    /// TODD: Might need to remove this class later
-    /// </summary>
-    public class MultipleChoreCompletion
-    {
-        //public int ChoreCompletionId { get; set; }
-        public int KidId { get; set; }
-        public List<int> ChoreItemIds { get; set; }
-        public int LocationId { get; set; }
-        public DateTime CompletionDateTime { get; set; }
-        //public string? Notes { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }
-        public decimal Price { get; set; }
-
-        public Kid? Kid { get; set; }
-        public ChoreItem? ChoreItem { get; set; }
-        public KidsChoreLocation? Location { get; set; }
-    }
 
 } 
